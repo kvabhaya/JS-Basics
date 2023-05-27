0437ddf7b59ae33452bb3fb97748449b6ea4927e
Kokila Abhayasundara <94110759+kvabhaya@users.noreply.github.com>
2023-05-27 09:51:59 +0530

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,38 @@
+function add(fn,sn) {
+    return fn+sn     
+ }
+console.log(add(10,2));
+
+
+function remainder(fn,sn) {
+    return fn%sn
+}
+console.log(remainder(10,3));
+
+
+
+//Calculater
+function add(fn,sn) {
+    return fn+sn     
+ }
+function sub(fn,sn) {
+    return fn-sn     
+ }
+function multi(fn,sn) {
+    return fn*sn     
+ }
+function div(fn,sn) {
+    return fn/sn     
+ }
+function rema(fn,sn) {
+    return fn%sn     
+ }
+function expo(fn,sn) {
+    return fn**sn
+}
+console.log(add(10,5));
+console.log(sub(10,5));
+console.log(multi(10,5));
+console.log(div(10,5));
+console.log(rema(10,5));
+console.log(expo(10,5));
